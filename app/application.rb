03d43032f83472844f543682d4274783aaa76d4b
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-23 12:36:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,7 @@ class Application
     
     if req.path.match(/cart/)
       if @@cart.empty? 
-        resp.write "Cart is empty"
+        resp.write "Your cart is empty"
       else @@cart.each do |item|
         resp.write "#{item}\n"
         end
